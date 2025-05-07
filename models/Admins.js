e83977d17cd04d1edb3d86342ca5b66c3e255834
Kouadio Bah Rodolphe <rodolphekouadio225@icloud.com>
2025-05-07 12:10:40 +0000

    Modifications prêtes pour le déploiement

--- models/Admins.js
+++
@@ -1,9 +0,0 @@
-// models/Admin.js
-const mongoose = require('mongoose');
-
-const adminSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // hashé
-});
-
-module.exports = mongoose.model('Admin', adminSchema);
